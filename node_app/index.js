974215783b099f805b3e2adac56b51ce3c5a7e2f
5163783 <5163783@liae.edu.uniube.br>
2025-09-09 20:29:29 -0300

    segundo commit

--- node_app/index.js
+++ node_app/index.js
@@ -16,11 +16,24 @@ app.get("/", (req, res) => {
   //res.json({ message: "Node.js está rodando no Docker!" });
   res.send("<h1>Hello World</h1>");
 });
+-
 
-app.get("/users", async (req, res) => {
+app.get("/api/v1/cliente", async (req, res) => {
   try {
     const connection = await mysql.createConnection(dbConfig);
-    const [rows] = await connection.execute("SELECT * FROM users");
+    const [rows] = await connection.execute("SELECT * FROM clientes");
+    await connection.end();
+    res.json(rows);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+
+app.get("/api/v1/cliente/:id", async (req, res) => {
+  try {
+    const connection = await mysql.createConnection(dbConfig);
+    const [rows] = await connection.execute("SELECT * FROM clientes where id = ?", [cliente]);
     await connection.end();
     res.json(rows);
   } catch (err) {
