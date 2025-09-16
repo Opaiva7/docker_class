a86a2f865a64531c7378104c3c14fe000f08b626
5163783 <5163783@liae.edu.uniube.br>
2025-09-16 20:14:33 -0300

    commit aula 16/09

--- node_app/index.js
+++ node_app/index.js
@@ -12,6 +12,57 @@ const dbConfig = {
   database: "appdb"
 };
 
+
+app.use(express.json());
+
+
+app.delete("/api/v1/cliente/:id", async (req, res) =>{
+
+try{
+
+    const cliente = req.params.id;
+    const connection = await mysql.createConnection(dbConfig);
+    const [rows] = await connection.execute("DELETE FROM clientes where id = ?", [cliente]);
+    await connection.end();
+    res.json(rows);
+  } catch (err) {
+      res.status(500).json({ error: err.message });
+  }
+});
+
+app.post("/api/v1/cliente", async (req, res) => {
+
+  try {
+    const cliente = req.body;
+
+    if(cliente.email == null || cliente.nome == null || cliente.celular == null){
+      res.status(500).json({ error: "Campos obrigatórios não foram preenchidos" });
+    }
+
+    const connection = await mysql.createConnection(dbConfig);
+    const [rows] = await connection.execute("INSERT INTO clientes (email, telefone, nome) VALUES (?, ?, ?)", [cliente.email, cliente.celular, cliente.nome]);
+    await connection.end();
+
+    if(result.affectedRows > 0){
+      res.json({ 
+        message: "Cliente inserido com sucesso",
+        id: result.insertId 
+      });
+    }else{
+      res.status(500).json({ error: "Erro ao inserir cliente" });
+    }
+
+  }catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+
+
+
+
+
+
 app.get("/", (req, res) => {
   //res.json({ message: "Node.js está rodando no Docker!" });
   res.send("<h1>Hello World</h1>");
